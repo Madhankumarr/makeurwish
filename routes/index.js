e32c425c1e2fde44953c35a1785588ae031dde90
madhankumar <madhankumarr@hotmail.com>
2014-12-31 02:34:57 +0530

    com

--- routes/index.js
+++ routes/index.js
@@ -330,7 +330,7 @@ try
 
                 var schedule = require('node-schedule');
                 var rule = new schedule.RecurrenceRule();
-                rule.minute =20;
+                rule.minute =6;
                 
 
                 var j = schedule.scheduleJob(rule, function(){
