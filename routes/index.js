a22a5b75ac6ef4fe94e491851f30c0a47e3b65b9
Madhankumar <madhankumarr@hotmail.com>
2015-01-16 21:25:26 +0530

    index time change

--- routes/index.js
+++ routes/index.js
@@ -351,8 +351,8 @@ try
                 var schedule = require('node-schedule');
                  var rule = new schedule.RecurrenceRule();
                   rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-                  rule.hour = 1;
-                  rule.minute =0;
+                  rule.hour = 15;
+                  rule.minute =55;
 
 
                 var j = schedule.scheduleJob(rule, function(){
